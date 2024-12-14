c9c11960d635dc6fae962925e57f89415ee6b0ae
tacheraSasi <tacherasasi@gmail.com>
2024-12-15 01:20:47 +0300

    minor changes

--- main.go
+++ main.go
@@ -7,17 +7,14 @@ import (
 	"github.com/ekilie/vintpm/toolkit"
 )
 
-// Entry point of the application
 func main() {
 	args := os.Args
 
-	// Ensure there's at least one argument
 	if len(args) < 2 {
 		printDefault()
 		return
 	}
 
-	// Handle command-line arguments
 	switch args[1] {
 	case "update-vint":
 		fmt.Println("Updating Vint...")
